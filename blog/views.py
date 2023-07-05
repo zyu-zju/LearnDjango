03cd3ac99c8bca539c1ba0755464bb48831cc52c
Zhihua Billy YU <zbillyyu@infinidat.com>
2023-07-05 19:28:26 +0800

    post_details

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,7 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
 from .models import Post
-
-posts = [
-	{
-		'author': 'CoreyMS',
-		'title': 'Blog Post 1',
-		'content':'First post content',
-		'date_posted': 'Jul 3, 2023'
-	},
-	{
-		'author': 'Billy YU',
-		'title': 'Blog Post 2',
-		'content':'second post content',
-		'date_posted': 'Jul 4, 2023'
-	}
-]
 
 # Create your views here.
 def home(request):
@@ -25,6 +11,15 @@ def home(request):
 	#return HttpResponse('<h1>Blog Home</h1>')
 	return render(request, 'blog/home.html', context)
 
+class PostListView(ListView):
+	model = Post
+	template_name = 'blog/home.html' # default to <app>/<model>_<viewtype>.html, which is 'blog/post_list.html'
+	context_object_name = 'posts'
+	ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+	model = Post
+	
 
 def about(request):
 #	return HttpResponse('<h1>Blog About</h1>')
