a38fb324fb03b780f13456a2886fa8534a2a22d7
Zhihua Billy YU <zbillyyu@infinidat.com>
2023-07-05 22:14:21 +0800

    Post create/update/delete

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+	ListView, 
+	DetailView,
+	CreateView,
+	UpdateView,
+	DeleteView
+)
 from .models import Post
 
 # Create your views here.
@@ -19,7 +26,41 @@ class PostListView(ListView):
 
 class PostDetailView(DetailView):
 	model = Post
-	
+
+
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+	model = Post
+	fields = ['title', 'content']
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+	model = Post
+	fields = ['title', 'content']
+
+	def form_valid(self, form):
+		form.instance.author = self.request.user
+		return super().form_valid(form)
+
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
+		
+		return False
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+	model = Post
+	success_url ='/'
+	def test_func(self):
+		post = self.get_object()
+		if self.request.user == post.author:
+			return True
+		
+		return False
 
 def about(request):
 #	return HttpResponse('<h1>Blog About</h1>')
